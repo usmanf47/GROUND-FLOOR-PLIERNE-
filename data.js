88602eef38d8ceb1cd5682e67f6ea6dc74755237
unknown <you@example.com>
2023-04-18 23:17:14 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,249 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entance-",
+      "name": "ENTANCE ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.07768420742492843,
+        "pitch": 0.07884281882959421,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2489715565990256,
+          "pitch": 0.03550127780601464,
+          "rotation": 17.27875959474387,
+          "target": "1-living-room"
+        },
+        {
+          "yaw": 0.5597704529635052,
+          "pitch": 0.009109882564029448,
+          "rotation": 1.5707963267948966,
+          "target": "2-reception"
+        },
+        {
+          "yaw": -3.0962144822475093,
+          "pitch": 0.033060848971652845,
+          "rotation": 0,
+          "target": "3-lobby-"
+        },
+        {
+          "yaw": -2.221479584082209,
+          "pitch": 0.030099322077242974,
+          "rotation": 1.5707963267948966,
+          "target": "1-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-room",
+      "name": "LIVING ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9098402064405775,
+          "pitch": 0.10144670225074037,
+          "rotation": 0.7853981633974483,
+          "target": "4-entance-"
+        },
+        {
+          "yaw": 2.08986434097209,
+          "pitch": 0.10775188659085977,
+          "rotation": 0,
+          "target": "3-lobby-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-reception",
+      "name": "RECEPTION",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07248247919784845,
+          "pitch": 0.09623177649298142,
+          "rotation": 0,
+          "target": "4-entance-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lobby-",
+      "name": "LOBBY ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.2060503058165768,
+        "pitch": 0.10001418046442723,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11692798829521145,
+          "pitch": 0.011154563019841746,
+          "rotation": 0,
+          "target": "0-entance-"
+        },
+        {
+          "yaw": -0.32942726397090283,
+          "pitch": 0.017267481125500694,
+          "rotation": 4.71238898038469,
+          "target": "1-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-entance-",
+      "name": "ENTANCE ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -3.065816852721577,
+        "pitch": 0.03898842926152035,
+        "fov": 1.3934346732050444
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.24552300461432885,
+          "pitch": 0.04371253552384857,
+          "rotation": 4.71238898038469,
+          "target": "1-living-room"
+        },
+        {
+          "yaw": -2.206140078426472,
+          "pitch": 0.09526024628612717,
+          "rotation": 1.5707963267948966,
+          "target": "1-living-room"
+        },
+        {
+          "yaw": -3.114185935096028,
+          "pitch": 0.07364225564833227,
+          "rotation": 0,
+          "target": "3-lobby-"
+        },
+        {
+          "yaw": 0.5684528068167545,
+          "pitch": 0.03919200875207807,
+          "rotation": 1.5707963267948966,
+          "target": "2-reception"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "GROUND FLOOR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
